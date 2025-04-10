b1969c72488e2e9c377612dbbe108acf461cd1d8
Noveryan Putra <noveryanpp@gmail.com>
2025-04-10 08:50:54 +0700

    migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('profile_picture')->default('user.png');
+            $table->string('nisn')->nullable();
+            $table->string('address');
+            $table->string('school')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
