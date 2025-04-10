492a9a8b6b369ce04537d1ed11efaf0c908e1a9f
Noveryan Putra <noveryanpp@gmail.com>
2025-04-10 10:00:30 +0700

    models

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'message',
+        'images',
+        'rating'
+    ];
 }
