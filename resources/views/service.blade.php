97f93e56bf3e83d5d281a43ab989c4b03847a82c
shfwnz <ilhamdzaky2007@gmail.com>
2025-04-21 19:57:15 +0700

    feat(prittier): add prittier

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -1,9 +1,16 @@
-<x-layouts.app :title="__('Service')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <livewire:service-card />
-            </div>
+<x-layouts.app :title="__('Services')">
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl bg-yellow-300">
+        <div class="grid auto-rows-min gap-4 md:grid-cols-3 bg-slate-400">
+            <livewire:service-card />
+            <livewire:service-card />
+            <livewire:service-card />
+            <livewire:service-card />
+            <livewire:service-card />
+            <livewire:service-card />
+            <livewire:service-card />
+            <livewire:service-card />
+            <livewire:service-card />
+            <livewire:service-card />
         </div>
     </div>
 </x-layouts.app>
