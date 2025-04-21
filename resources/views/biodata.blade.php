3fef992800d7f4ecdc10da641ff232b8bd8907b5
shfwnz <ilhamdzaky2007@gmail.com>
2025-04-21 20:40:56 +0700

    fix(dependencies): prettier

--- resources/views/biodata.blade.php
+++ resources/views/biodata.blade.php
@@ -1,9 +1,7 @@
 <x-layouts.app :title="__('biodata')">
-
     <head>
-        <h1 class='p-2  text-2xl font-black '>Biodata </h1>
-        <p class='p-2 text-orange-400'>dashboard/account/profile</p>
-
+        <h1 class="p-2 text-2xl font-black">Biodata</h1>
+        <p class="p-2 text-orange-400">dashboard/account/profile</p>
     </head>
-    <hr>
+    <hr />
 </x-layouts.app>
