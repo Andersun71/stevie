e6dd10485fc9ca57650f36f814313b6d8d108edf
shfwnz <ilhamdzaky2007@gmail.com>
2025-04-23 14:45:39 +0700

    fix(database): connect to database

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,10 +1,3 @@
 <x-layouts.app :title="__('Products')">
-    <div class="flex h-auto w-auto flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3 max-lg:grid-cols-1">
-            <livewire:product-card />
-            <livewire:product-card />
-            <livewire:product-card />
-            <livewire:product-card />
-        </div>
-    </div>
+    <livewire:product-card />
 </x-layouts.app>
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -1,16 +1,3 @@
 <x-layouts.app :title="__('Services')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3 max-lg:grid-cols-1">
-            <livewire:service-card />
-            <livewire:service-card />
-            <livewire:service-card />
-            <livewire:service-card />
-            <livewire:service-card />
-            <livewire:service-card />
-            <livewire:service-card />
-            <livewire:service-card />
-            <livewire:service-card />
-            <livewire:service-card />
-        </div>
-    </div>
+    <livewire:product-card />
 </x-layouts.app>
