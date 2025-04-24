1f39ce7d1468bc0abe2585f5b02b751541f13e73
shfwnz <ilhamdzaky2007@gmail.com>
2025-04-24 22:07:58 +0700

    feat(product): add fallback

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,25 +1,42 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-2">
-            <div class="p-4 relative h-30 overflow-hidden rounded-xl bg-[#FCEFE4]">
+            <div
+                class="p-4 relative h-30 overflow-hidden rounded-xl bg-[#FCEFE4]"
+            >
                 <p class="text-black font-semibold text-2xl">Lorem Ipsum</p>
             </div>
-            <div class="p-4 relative h-30 overflow-hidden rounded-xl bg-[#EAE2B7]">
+            <div
+                class="p-4 relative h-30 overflow-hidden rounded-xl bg-[#EAE2B7]"
+            >
                 <p class="text-black font-semibold text-2xl">Lorem Ipsum</p>
             </div>
         </div>
         <div class="flex gap-4">
             <div class="flex flex-1 gap-4">
-                <div class="p-3 flex-1 relative overflow-hidden rounded-xl bg-[#F77F00]">
+                <div
+                    class="p-3 flex-1 relative overflow-hidden rounded-xl bg-[#F77F00]"
+                >
                     <p class="text-black font-semibold text-2xl">Lorem Ipsum</p>
                 </div>
-                <div class="p-3 flex-1 relative overflow-hidden rounded-xl bg-[#C8F0D4]">
+                <div
+                    class="p-3 flex-1 relative overflow-hidden rounded-xl bg-[#C8F0D4]"
+                >
                     <p class="text-black font-semibold text-2xl">Lorem Ipsum</p>
                 </div>
             </div>
 
-            <div class="p-5 relative h-90 flex-1 overflow-hidden rounded-xl bg-[#FDF8E4]">
-                <p class="text-black font-semibold text-2xl break-words">What We do?</br> Get to Know </br> Us</p>
+            <div
+                class="p-5 relative h-90 flex-1 overflow-hidden rounded-xl bg-[#FDF8E4]"
+            >
+                <p class="text-black font-semibold text-2xl break-words">
+                    What We do?
+
+                    <br />
+                    Get to Know
+                    <br />
+                    Us
+                </p>
             </div>
         </div>
     </div>
