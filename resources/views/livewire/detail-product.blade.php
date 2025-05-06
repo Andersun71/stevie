3d2da900cd53abf4f9f91623c0e0adf8fe632a91
shfwnz <ilhamdzaky2007@gmail.com>
2025-05-06 08:06:58 +0700

    fix(reviews): improve reviews

--- resources/views/livewire/detail-product.blade.php
+++ resources/views/livewire/detail-product.blade.php
@@ -34,28 +34,21 @@
                     <flux:button class="w-full">show review</flux:button>
                 </flux:modal.trigger>
 
-                <flux:modal name="show-review" class="md:w-96">
+                <flux:modal name="show-review" class="w-full">
                     <div class="space-y-6">
                         <div>
-                            <flux:heading size="lg">
-                                Update profile
-                            </flux:heading>
-                            <flux:text class="mt-2">
-                                Make changes to your personal details.
+                            <flux:heading size="lg">Reviews</flux:heading>
+                            <flux:text class="mt-2 lowercase">
+                                Your review makes our product better
                             </flux:text>
-                        </div>
-
-                        <flux:input label="Name" placeholder="Your name" />
-
-                        <flux:input label="Date of birth" type="date" />
-
-                        <div class="flex">
-                            <flux:spacer />
-
-                            <flux:button type="submit" variant="primary">
-                                Save changes
+                            <flux:button class="mt-2 lowercase">
+                                Write a review
                             </flux:button>
                         </div>
+
+                        <flux:separator />
+
+                        <div class="w-full bg-yellow-200">apa</div>
                     </div>
                 </flux:modal>
             </div>
